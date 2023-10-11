35b09b3dd81fccb49d8c52e4d320f78c41f19b13
boybenson <ybenson96@gmail.com>
2023-10-11 18:37:46 +0000

    feat: changes

---
+++ src/component/Barchart/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Dataset",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [10, 6, 30, 10, 9, 14, 4, 8, 13, 10, 7, 12],
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: [13, 8, 5, 15, 12, 15, 7, 3, 13, 10, 11, 6],
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+const BarChart = () => {
+  return <Bar options={options} data={data} />;
+};
+
+export default BarChart;
---
+++ src/data/admin-urls.ts
@@ -0,0 +1,44 @@
+import {
+  CurrencyBangladeshiIcon,
+  InboxStackIcon,
+  ShoppingCartIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+
+export const AdminUrls = [
+  {
+    label: "Dashboard",
+  },
+  {
+    label: "Products",
+  },
+  {
+    label: "Orders",
+  },
+  {
+    label: "Broadcast Messages",
+  },
+];
+
+export const dashboardData = [
+  {
+    label: "Total Revenue",
+    value: `GHC 300`,
+    Icon: CurrencyBangladeshiIcon,
+  },
+  {
+    label: "Total Customers",
+    value: `180`,
+    Icon: UserGroupIcon,
+  },
+  {
+    label: "Total Products",
+    value: `450`,
+    Icon: InboxStackIcon,
+  },
+  {
+    label: "Total Orders",
+    value: `100`,
+    Icon: ShoppingCartIcon,
+  },
+];
